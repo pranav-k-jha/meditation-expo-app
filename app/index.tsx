75db1074f4289789a10ac5c007e8e7072dadb2e1
pranav-k-jha <pranav.jha@mail.concordia.ca>
2024-11-25 23:25:44 -0500

    fixed bug in tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import beachImage from "@/assets/meditation-images/beach.webp";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
 
 const App = () => {
   return (
@@ -17,14 +18,21 @@ const App = () => {
           className="flex-1"
           colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
         >
-          <SafeAreaView className="flex-1 px-1 justify-between">
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
             <View>
               <Text className="text-center text-white font-bold text-4xl mt-4">
                 Simple Meditation
               </Text>
               <Text className="text-center text-white text-2xl mt-3">
-                Simplifying Meditation for Everyone
+                Simplifying Meditation for Everyone out there
               </Text>
+            </View>
+
+            <View>
+              <CustomButton
+                onPress={() => console.log("tap")}
+                title="Get Started"
+              />
             </View>
             <StatusBar style="light" />
           </SafeAreaView>
